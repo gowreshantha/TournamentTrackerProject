1ba7c4ac042fc32153ac3d57cbb6dfa08ec8db34
Gowresh N <gowreshantha@gmail.com>
2022-06-08 21:54:53 +0530

    Added comments

--- TrackerLibrary/PersonModel.cs
+++ TrackerLibrary/PersonModel.cs
@@ -6,11 +6,29 @@ using System.Threading.Tasks;
 
 namespace TrackerLibrary
 {
+    /// <summary>
+    /// Represents one person
+    /// </summary>
     public  class PersonModel
     {
+        /// <summary>
+        /// The first name of the person.
+        /// </summary>
         public string FirstName { get; set; }
+
+        /// <summary>
+        /// The last name of the person.
+        /// </summary>
         public string LastName { get; set; }
+
+        /// <summary>
+        /// The email address of the person.
+        /// </summary>
         public string Email { get; set; }
+
+        /// <summary>
+        /// The cell phone number of the person.
+        /// </summary>
         public string CellPhone { get; set; }
     }
 }
