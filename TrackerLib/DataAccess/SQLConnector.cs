67d05c53a523b0c98b43c6e6375c030a16422c56
Gowresh N <gowreshantha@gmail.com>
2022-06-20 17:02:17 +0530

    SQL connection successfull

--- TrackerLib/DataAccess/SQLConnector.cs
+++ TrackerLib/DataAccess/SQLConnector.cs
@@ -16,21 +16,21 @@ namespace TrackerLib.DataAccess
         /// <param name="model">The prize information.</param>
         /// <returns>The prize information, including the unique identifier.</returns>
         public PrizeModel CreatePrize(PrizeModel model)
-        {
+        {        
             using (IDbConnection connection = new System.Data.SqlClient.SqlConnection(GlobalConfig.GetConnectonString("Tournaments")))
             {
                 var p = new DynamicParameters();
-                p.Add("@PlaceNumber",model.PlaceNumber);
+                p.Add("@PlaceNumber", model.PlaceNumber);
                 p.Add("@PlaceName", model.PlaceName);
-                p.Add("@PrizeAmount",model.PrizeAmount);
-                p.Add("@PrizePercentage",model.PrizePercentage);
+                p.Add("@PrizeAmount", model.PrizeAmount);
+                p.Add("@PrizePercentage", model.PrizePercentage);
                 p.Add("@id", 0, dbType: DbType.Int32, direction: ParameterDirection.Output);
 
                 connection.Execute("dbo.spPrizes_Insert", p, commandType: CommandType.StoredProcedure);
 
                 model.Id = p.Get<int>("@id");
 
-                return model; 
+                return model;
             }
         }
     }
